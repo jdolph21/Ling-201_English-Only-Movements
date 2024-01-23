acbb0bd1987235adde429bf97c16cb6aa7cb4a21
jdolph21 <joshuadolph21@augustana.edu>
2024-01-23 15:38:20 -0600

    Modified the layout of the Quiz page

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var answerMap = new Map();
 var correctAnswerMap = new Map();
 correctAnswerMap.set("answer1", "None+of+the+Above");
 correctAnswerMap.set("answer2", "31-40");
+correctAnswerMap.set("answer3", "0-5");
+correctAnswerMap.set("answer4", "350%2B");
+correctAnswerMap.set("answer5", "60%2B+million");
 
 var numCorrect = 0;
 var numQuestions = 0;
@@ -23,8 +26,9 @@ function answerHandling() {
 	var feedbackMap = new Map();
 	feedbackMap.set("answer1", "The United States does not have an official language");
 	feedbackMap.set("answer2", "32 States (and all 5 territories) have English as (at least one of) their official language(s)");
-	
-
+	feedbackMap.set("answer3", "3 States have more than one official languages: South Dakota, Hawaii, and Alaska");
+	feedbackMap.set("answer4", "Over 350 unique languages are spoken within the United States");
+	feedbackMap.set("answer5", "Over 67.8 million people in the US are native speakers of a language other than English, as of 2019; the number is higher now");
 	
 	for (var item of feedbackMap.keys()) {
 		numQuestions++;
