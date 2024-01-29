3804160589ca37487a91f2ef16e7ea82d228e218
IsaacMcConnell <isaacmcconnell21@augustana.edu>
2024-01-29 01:28:59 -0600

    Fixed title HTML tags and deleted a couple more files that were unused.

--- results.php
+++ results.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
-    <title>Quiz</title>
+    <title>Quiz Results | English Only Movement</title>
     <link href="style.css" rel="stylesheet" type="text/css" />
   </head>
   
